eb8d48598ab6388b1a9e71b4042e8dfe73dfe86e
Cristina Nguyen <c.nguyen0994@gmail.com>
2021-04-21 21:39:03 -0700

    [Queue] add queue data from API and display on component

--- rockbot/src/components/Queue.vue
+++ rockbot/src/components/Queue.vue
@@ -3,10 +3,37 @@
     and the Queue component below it -->
     <div id="Queue">
         <h4>This is the queue component</h4>
+        {{music.artist}}
     </div>
 </template>
 
 <script>
+export default {
+//   el: '#nowPlaying1', el only needed during new vue instance creation
+  data() {
+    return {
+      music: null
+    }
+  },
+  mounted() {
+    fetch("https://api.rockbot.com/v3/engage/now_playing?queue=1", {
+    method: "GET",
+    headers: {
+      Authorization: "2ab742c917f872aa88644bc8f995e03159b2"
+    }
+  })
+    .then(res => res.json())
+    .then(response => (this.music = response.response.queue[0]))
+    //loop through queue and display the artist and small artwork
+    //include button to like the artist
+    .then(response => {
+      console.log(response.response.queue[0]);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+  }
+}
 </script>
 
 <style>
