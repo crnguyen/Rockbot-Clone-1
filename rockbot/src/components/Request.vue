e0505d12aae1cd6477a3b2fb73540f54f61bfac8
Cristina Nguyen <c.nguyen0994@gmail.com>
2021-04-28 20:12:24 -0700

    [Request] include fetch API call to search artists and display on component

--- rockbot/src/components/Request.vue
+++ rockbot/src/components/Request.vue
@@ -2,12 +2,47 @@
     <div id="request">
         <h3>Search</h3>
         <div class="searchBar">
-            <input type="text" placeholder="Search artist">
-        </div>     
+            <input type="text" v-model="search" placeholder="Search artist">
+        </div> 
+        <button
+        type="submit"
+        v-on:click="getData">submit
+        </button>
+
+        <div v-for="(artist, index) in artist" :key="index">
+            {{artist.artist}}
+        </div>
     </div>
 </template>
 
 <script>
+// request artist 
+export default {
+//   el: '#nowPlaying1', el only needed during new vue instance creation
+  data() {
+    return {
+      artist: []
+    }
+  },
+  methods: {
+      getData: function(){
+          fetch(`https://api.rockbot.com/v3/engage/search_artists?query=${this.search}`, {
+              method: "GET",
+              headers: {
+                  Authorization: "2ab742c917f872aa88644bc8f995e03159b2"
+            }
+        })
+        .then(res => res.json())
+        .then(response => (this.artist = response.response))
+        .then(response => {
+            console.log("search response:", response);
+        })
+        .catch(err => {
+            console.log("search error:", err);
+        })
+        }
+    }
+}
 </script>
 
 <style>
